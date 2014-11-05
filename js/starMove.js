9f573a7713fff404b74e4d3cb66bf93bb79a9d10
zhaodifont <zhaodifont@126.com>
2014-11-05 18:10:05 +0800

    ads

--- js/starMove.js
+++ js/starMove.js
@@ -28,3 +28,51 @@ function $(a){return document.getElementById(a)};
 function getStyle(obj,tagName){
 	return obj.currentStyle?obj.currentStyle[tagName]:getComputedStyle(obj,false)[tagName];
 }
+
+// nav 移动下标
+
+function add_navicon(obj){ // 目前为id
+	this.obj = obj;
+	this.aLis = this.obj?this.obj.getElementsByTagName('li'):console.log('请输入有效节点');
+}
+add_navicon.prototype.init = function(){
+	this.addHanles_li();
+}
+add_navicon.prototype.addHanles_li = function(){
+	var _this = this;
+	var icon =document.createElement('div');
+	icon.className="lit_a";
+	this.obj.insertBefore(icon,this.obj.firstChild);
+	/*init style*/
+	icon.style.left = this.aLis[0].offsetLeft + parseInt(this.aLis[0].clientWidth/2) -icon.offsetWidth/2+ "px";
+	this.aLis[0].getElementsByTagName('a')[0].style.color = "#ffe40f";
+
+	for (var i = 0; i < this.aLis.length; i++) {
+		this.aLis[i].onmouseover=function(){
+			starMove(icon,{left:this.offsetLeft + parseInt(this.clientWidth/2) -icon.offsetWidth/2});
+			for(var o=0;o<_this.aLis.length;o++){
+				_this.aLis[o].getElementsByTagName('a')[0].style.color = "#fff";
+			}
+			this.getElementsByTagName('a')[0].style.color = "#ffe40f";
+		}
+	};
+}
+
+
+// 添加 blind
+
+function blindBtn(faParent,btn){
+	var a = true;
+	var oldw = faParent.offsetWidth;
+	btn.onclick=function(){
+		if(a){
+			starMove(faParent,{width:this.offsetWidth})
+			a = false;
+			console.log(a)
+		}
+		else if(!a){
+			starMove(faParent,{width:oldw})
+			a = true;
+		}
+	}
+}

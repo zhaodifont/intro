2e593c1577d4f3009321ca94e5a12d62ad45dd39
zhaodifont <zhaodifont@126.com>
2016-10-30 22:45:04 +0800

    m1

--- personal/src/js/main.js
+++ personal/src/js/main.js
@@ -1,14 +1,28 @@
 window.onload = function(){
 
+
+function Anim(obj,x){
+	obj.removeClass(x + ' animated').addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+		console.log(1)
+	  $(this).removeClass(x + ' animated');
+
+	});
+};
+Anim($('.welcome'),'hinge');
+
 $('#container').fullpage({
 	scrollingSpeed:800,
-	navigation: true,
+	// navigation: true,
+	verticalCentered:true,
 	css3:true,
 	loopHorizontal:false,
-	'navigation': true,
+	// 'navigation': true,
 	anchors: ['page1', 'page2'],
 	menu: '#menu',
     afterLoad: function(anchorLink, index){
+    	if(index == 1){
+    		Anim($('.welcome'),'bounceIn')
+    	}
 
 	},
 	onLeave: function(index, direction){
